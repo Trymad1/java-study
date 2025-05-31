462dd2c6208509b335c224cbcb1c648ec450c08d
trymad <trymad@gmail.com>
2025-05-31 11:18:56 +0300

    sdfsdf

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+        String[] arr = new String[3];
 
-        // примитивы
-		boolean bol = true; // true / false
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
 
-		// short b = 2; // -32768 - 32767
-		byte a = 1; // -128 - 127
-		int c = 3; // -2147483648 - 2147483647
-		long d = 4L; // -9223372036854775808 - 9223372036854775807
-		double f = 6.0; // // 4.9 × 10⁻³²⁴ - // 1.8 × 10³⁰⁸
-		float e = 5.0f; // 1.4 × 10⁻⁴⁵ - 3.4 × 10³⁸
+        Dog dog = new Dog("dogname");
+        Cat cat = new Cat("catname");
 
-		char g = 'A';
-		char h = 65;
-		
-		// ссылочные типы данных
-		String str = "Hello";
-		String nullable = null;
+        Animal animal = new Dog("animalname");
 
     }
 }
